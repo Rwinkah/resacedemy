744daf8f6cc71a513ab652b9d8730e46babc5f55
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-07-18 04:07:15 +0100

    facilities section

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import AboutSection from "./AboutSection";
+import FacilitiesSection from "./FacilitiesSection";
 
 const Main = () => {
 	return (
 		<>
 			<Hero />
 			<AboutSection />
+			<FacilitiesSection />
 		</>
 	);
 };
